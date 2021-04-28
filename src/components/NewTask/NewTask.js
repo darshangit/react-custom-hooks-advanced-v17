29fe5fc3ca942147c88d04864d825194c7b2f47c
Darshan Satya <darshans1989@gmail.com>
2021-04-27 21:04:18 -0400

    Converting the loading list to custom hook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { FIREBASE_URL } from "../../config";
 
-import Section from '../UI/Section';
-import TaskForm from './TaskForm';
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -11,19 +12,16 @@ const NewTask = (props) => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+      const response = await fetch(FIREBASE_URL + "tasks.json", {
+        method: "POST",
+        body: JSON.stringify({ text: taskText }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       if (!response.ok) {
-        throw new Error('Request failed!');
+        throw new Error("Request failed!");
       }
 
       const data = await response.json();
@@ -33,7 +31,7 @@ const NewTask = (props) => {
 
       props.onAddTask(createdTask);
     } catch (err) {
-      setError(err.message || 'Something went wrong!');
+      setError(err.message || "Something went wrong!");
     }
     setIsLoading(false);
   };
